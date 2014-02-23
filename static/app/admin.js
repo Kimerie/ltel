3f5bf9c69a004df5bd7ab33e684ba7752c9d4d6b
nick w <nickweinberg90@gmail.com>
2014-02-23 11:53:10 -0800

    create barchart directive.

--- static/app/admin.js
+++ static/app/admin.js
@@ -1,4 +1,23 @@
+cdexApp.directive('barchart', function() {
+  return {
+    // required to make it work as an element
+    restrict: 'E',
+
+    // replace <photo> with this html
+    template: '<div>YO</div>',
+    replace: true,
+
+    // observe and manipulate the DOM
+    link: function($scope, element, attrs) {
+      console.log('sup');
+
+    }
+  }
+})
+
+
 cdexApp.controller('AdminController', function ($scope) {
 
+  console.log('test');
 
 });
